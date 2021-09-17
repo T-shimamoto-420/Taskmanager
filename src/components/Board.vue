b4ca8036737a11694a27437c8775443762aab47c
T-shimamoto-420 <t12412@icloud.com>
2021-09-17 10:20:27 +0900

    first-commit

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <header>
-      my Trello
+      my Tasks
     </header>
     <main>
-      <p class="info-line">All: {{ totalCardCount }} tasks</p>
       <draggable :list="lists"
             @end="movingList"
             class="list-index">
         <list v-for="(item, index) in lists" :key="item.id" :title="item.title" :cards="item.cards" :listIndex="index" @change="movingCard"/>
+        <br>
         <list-add/>
       </draggable>
     </main>
